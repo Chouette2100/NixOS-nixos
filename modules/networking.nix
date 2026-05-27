4045c300c63721f7c8ca4af9412316944fec3af7
Chouette2100 <your-email@example.com>
2026-05-28 08:49:07 +0900

    feat(networking): add static host mappings via networking.hosts
    Add declarative host-to-IP entries converted from dnsmasq address rules.

--- modules/networking.nix
+++ modules/networking.nix
@@ -7,6 +7,23 @@ in
 {
   networking.networkmanager.enable = true;
   networking.nftables.enable = true;
+  networking.hosts = {
+    "192.168.0.13" = [ "Mint221BE" ];
+    "192.168.0.23" = [ "LB10" ];
+    "192.168.0.16" = [ "opi" ];
+    "192.168.0.21" = [ "opiwf" ];
+    "192.168.0.24" = [ "LMTabKS" ];
+    "192.168.0.22" = [ "LMTabKSw" ];
+    "192.168.0.28" = [ "fbsd142" "ubuntu05" ];
+    "192.168.0.29" = [ "obsd77" ];
+    "192.168.0.19" = [ "ubuntu02" ];
+    "192.168.0.27" = [ "ubuntu04" ];
+    "192.168.122.21" = [ "Mint213A" ];
+    "192.168.122.30" = [ "arch00" ];
+    "192.168.122.84" = [ "qfbsd143" ];
+    "133.18.160.207" = [ "kagoya10" ];
+    "49.212.207.19" = [ "sakura" ];
+  };
   
   # QEMU/KVM
   fileSystems."/mnt/nfsh" = lib.mkIf isQemu {
