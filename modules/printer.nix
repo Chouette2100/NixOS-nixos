748f03c2c7a04beb5a3717e9a86937993fb259e3
Chouette2100 <your-email@example.com>
2026-08-08 15:27:42 +0900

    174 install printer.nix(ipp://mint223:631/printers/DCP-J925N)

--- modules/printer.nix
+++ modules/printer.nix
@@ -17,7 +17,7 @@
         name = "mint223ipp";
         location = "mint223";
         # CUPS Web UIで確認したプリンタ名をURIエンコードして指定
-        deviceUri = "ipp://mint223/ipp/";
+        deviceUri = "ipp://mint223:631/printers/DCP-J925N";
         # IPP Everywhere対応プリンタなら "everywhere" でOK
         model = "everywhere";
         ppdOptions = {
